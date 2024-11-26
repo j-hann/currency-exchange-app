2c2eb797eebe3e1ea788a2d36fa90c09b5b0f802
j-hann <j.han77055@gmail.com>
2024-11-27 03:35:02 +0900

    🎉 Begin : 프로젝트 생성
    화폐 환전 앱 프로젝트 생성

---
+++ src/main/java/com/currency/currencyexchangeappproject/CurrencyExchangeAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.currency.currencyexchangeappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currency/currencyexchangeappproject/CurrencyExchangeAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency.currencyexchangeappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
