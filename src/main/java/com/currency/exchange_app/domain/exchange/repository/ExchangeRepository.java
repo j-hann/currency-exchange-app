ceea5206b5f757be8a4a56a97844d500f17f1ecb
j-hann <j.han77055@gmail.com>
2024-12-27 13:03:49 +0900

    ♻️ Refactor : 성능 최적화 & 정렬
    유저 환전 요청 전체 조회시 발생하는 n+1 문제 해결

--- src/main/java/com/currency/exchange_app/domain/exchange/repository/ExchangeRepository.java
+++ src/main/java/com/currency/exchange_app/domain/exchange/repository/ExchangeRepository.java
@@ -13,7 +13,12 @@ import java.util.List;
 @Repository
 public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
 
-    // userId로 Exchange를 조회
+    // userId로 Exchange를 조회 - 최신순 정렬
+    @Query("SELECT e "
+        + "FROM Exchange e "
+        + "JOIN FETCH e.user "
+        + "JOIN FETCH e.currency "
+        + "ORDER BY e.id DESC")
     List<Exchange> findByUserId(Long userId);
 
     default Exchange findByExchangeIdOrElseThrow(Long exchangeId){
